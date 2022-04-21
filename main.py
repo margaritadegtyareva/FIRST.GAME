088d6065e451c01e29fd681cdeec04ef84aff735
margarita <skywind2267@gmail.com>
2022-04-21 21:22:29 +0500

    сделали макет

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 import pygame
-print(1)
+import random
+
+WIDTH = 360  # ширина игрового окна
+HEIGHT = 480 # высота игрового окна
+FPS = 30 # частота кадров в секунду
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    # Рендеринг
+    screen.fill(WHITE)
+    # после отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+    clock.tick(FPS)
